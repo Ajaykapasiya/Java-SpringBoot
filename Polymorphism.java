e15497d06366833ebdc1f56913f250959631d3d6
ajay <ajaykumar290904@gmail.com>
2025-12-19 02:25:12 +0530

    Implement animalSound method for Dog, Cat, and Bird classes in Polymorphism example

--- Polymorphism.java
+++ Polymorphism.java
@@ -6,6 +6,30 @@ class Animal{
 
  class Bird extends Animal {
     public void animalSound(){
-      
+      System.out.println("Bird say wee wee");
+    }
+}
+
+class Dog extends Animal{
+    public void animalSound(){
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal{
+    public void animalSound(){
+        System.out.println("Cat mee mee");
+    }
+}
+
+public class Polymorphism{
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        Animal Bird = new Bird();
+        Animal Cat = new Cat();
+
+        myDog.animalSound();
+        Bird.animalSound();
+        Cat.animalSound();
     }
 }
