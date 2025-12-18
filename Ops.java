33cab8d78ddd67a2ab0fe14f7415a7e904d5ba5e
ajay <ajaykumar290904@gmail.com>
2025-12-18 12:54:11 +0530

    Add Pen class and update Ops.java for object instantiation

--- Ops.java
+++ Ops.java
@@ -1,8 +1,16 @@
-public class Ops {
-    int x = 5;
+class Pen{
+    String name;
+    String type;
 
-    public static void main(String[] args) {
-    Ops myObj = new Ops();
-    System.out.println(myObj.x);
-    }
+   public void write(){
+    System.out.println("Write");
+   }
+}
+
+public class Ops {
+   static  void main(String[]args){
+      Pen pen1 = new Pen();
+      pen1.name = "Pilot";
+      pen1.type = "ballpoint";
+   }
 }
