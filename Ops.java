b479173af303a26f2dab1e0786f67573c26cdd45
ajay <ajaykumar290904@gmail.com>
2025-12-18 13:22:23 +0530

    Refactor Ops.java to improve method visibility and enhance Pen class functionality

--- Ops.java
+++ Ops.java
@@ -5,12 +5,22 @@ class Pen{
    public void write(){
     System.out.println("Write");
    }
+   public void printColour(){
+    System.out.println(this.name);
+   }
+   public void printType(){
+    System.out.println(this.type);
+   }
 }
 
 public class Ops {
-   static  void main(String[]args){
+   public static  void main(String[]args){
       Pen pen1 = new Pen();
       pen1.name = "Pilot";
-      pen1.type = "ballpoint";
+      pen1.type = "Ballpoint";
+
+      pen1.write();
+      pen1.printColour();
+      pen1.printType();
    }
 }
