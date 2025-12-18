eb32cf7877a57ea6c280aadd662ae37cd4b76c22
ajay <ajaykumar290904@gmail.com>
2025-12-19 03:15:27 +0530

    Add Shape and Triangle classes to demonstrate inheritance in Java

---
+++ Inheritance.java
@@ -0,0 +1,16 @@
+class Shape{
+    String colour;
+}
+
+class Triangle extends Shape{
+
+}
+
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.colour = "Blue";
+        System.out.println(t1.colour);
+    }
+}
