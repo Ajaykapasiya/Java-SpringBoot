60debd04b130698f4b624545b54e7b5312fe1ad1
ajay <ajaykumar290904@gmail.com>
2025-12-23 18:26:59 +0530

    Refactor MainClass and Person class; implement constructor for Person and update main method for object creation

--- MainClass.java
+++ MainClass.java
@@ -1,19 +1,51 @@
 
 
-public class MainClass {
-    public static void main(String[]args){
-        Person p1 = new Person();
-        p1.name = "Sam";
-        p1.age = 22;
-        System.out.println(p1.name + " " + p1.age);
+// public class MainClass {
+//     public static void main(String[]args){
+//         Person p1 = new Person();
+//         p1.name = "Sam";
+//         p1.age = 22;
+//         System.out.println(p1.name + " " + p1.age);
 
-        // Person p2 = new Person();
-        // p2.name = "rick";
-        // p2.age = 43;
-    }
+//         Person p2 = new Person();
+//         p2.name = "rick";
+//         p2.age = 43;
+//         System.out.println(p2.name + " " + p2.age);
+
+//         p1.walk();
+//         p2.eat();
+//     }
     
-}
+// }
+// class Person{
+//     String name;
+//     int age;
+
+//     void walk(){
+//         System.out.println(name + " is walking");
+//     }
+
+//     void eat(){
+//         System.out.println(name + " is eating");
+//     }
+// }
+
+
 class Person{
     String name;
     int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class MainClass {
+  
+    public static void main(String[] args) {
+        Person p1 = new Person("Rohit", 33);
+        System.out.println(p1.name + " " + p1.age);
+    }
+    
 }
